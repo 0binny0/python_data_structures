1ce8deeedd9499f596cbc788ecc6ec26fe5f43fb
Ben Hedgepeth <benhh@gmx.com>
2023-02-25 22:02:45 -0800

    Tested .append(data) method

--- linked_list/main.py
+++ linked_list/main.py
@@ -3,3 +3,34 @@ class LinkedList:
 
     def __init__(self):
         self.head = None
+
+    def prepend(self, data):
+        '''Sets the head of the linked list'''
+        node = self.Node(data)
+        if self.head is None:
+            self.head = node
+        else:
+            old_head = self.head
+            node.next = old_head
+            self.head = node
+        return self.head
+
+    def append(self, data):
+        node = self.Node(data)
+        current = self.head
+        while True:
+            if current.next is None:
+                current.next = node
+                return node
+            current = current.next
+
+
+
+    class Node:
+
+        def __init__(self, data):
+            self.data = data
+            self.next = None
+
+        def __str__(self):
+            return f"Node ({self.data})"
--- linked_list/tests.py
+++ linked_list/tests.py
@@ -1,5 +1,6 @@
 
 from unittest import TestCase, main
+from unittest.mock import patch
 
 from main import LinkedList
 
@@ -11,7 +12,39 @@ class TestLinkedListHead(TestCase):
 
     def test_linked_list_head_is_none(self):
         self.assertIsNone(self.linked_list.head, None)
-        
+
+
+class TestPrependNodeToLinkedList(TestCase):
+
+    def setUp(self):
+        self.initial_head = LinkedList.Node(1)
+        linked_list = LinkedList()
+        linked_list.head = self.initial_head
+        self.updated_head = linked_list.prepend(2)
+
+    def test_prepend_sets_new_head(self):
+        self.assertEqual(self.updated_head.data, 2)
+        self.assertEqual(self.updated_head.next.data, 1)
+
+
+class TestAppendNodeToLinkedList(TestCase):
+
+
+    def setUp(self):
+        self.linked_list = LinkedList()
+        self.mock_head_config = {
+            "data": 1
+        }
+
+    def test_appended_node(self):
+        with patch.object(self.linked_list, 'head', **self.mock_head_config) as mock_head:
+            mock_head.next = None
+            node = self.linked_list.append(2)
+            self.assertEqual(self.linked_list.head.data, 1)
+            self.assertEqual(self.linked_list.head.next.data, 2)
+            self.assertIsNone(self.linked_list.head.next.next)
+
+
 
 if __name__ == "__main__":
     main()
