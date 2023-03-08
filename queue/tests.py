8e196ec8c5eb41a2ad64b850190e948242198a32
Ben Hedgepeth <benhh@gmx.com>
2023-03-08 11:40:35 -0800

    added Queue.enqueue() method

---
+++ queue/tests.py
@@ -0,0 +1,17 @@
+
+from unittest import TestCase, main
+
+from main import Queue
+
+class TestSeekOperation(TestCase):
+
+    def setUp(self):
+        self.queue = Queue()
+        self.queue.enqueue(1)
+
+    def test_head_node_value(self):
+        self.assertEqual(self.queue.head.data, 1)
+        self.assertIsNone(self.queue.head.next)
+
+if __name__ == "__main__":
+    main()
